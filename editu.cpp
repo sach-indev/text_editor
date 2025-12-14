ef3c2b12bfe5b9a99d523b83c922be94752b7dca
Sachin Choudhary <sachinchoudharyboss24@gmail.com>
2025-12-14 19:10:29 +0530

    Too RawMode we go

--- editu.cpp
+++ editu.cpp
@@ -1,6 +1,70 @@
-#include <iostream>
+/*Includes*/
+
+#include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/*Data*/
+
+struct termios orig_termios;
+
+/*Terminal*/
+
+void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode()
+{
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    {
+        die("tcsetattr");
+    }
+}
+
+void enableRawMode()
+{
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcsgetattr");
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+/*init*/
 
 int main()
 {
-    return 229;
+    enableRawMode();
+
+    while(1)
+    {
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+
+        if(iscntrl(c))
+        {
+            printf("%d\r\n", c);
+        }
+
+        else
+        {
+            printf("%d ('%c')\r\n", c, c);
+        }
+
+        if(c=='q') break;
+    }
+    return 0;
 }
